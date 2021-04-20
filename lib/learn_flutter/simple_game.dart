1b74bd86779a4a6f6fbb7c876f70b0deb48af18e
seongyunLim <lsy96999@gmail.com>
2021-04-20 17:02:12 +0900

    cardmove and cardwidget

--- lib/learn_flutter/simple_game.dart
+++ lib/learn_flutter/simple_game.dart
@@ -8,19 +8,105 @@ class SimpleGame extends StatefulWidget {
 
 class _SimpleGameState extends State<SimpleGame> {
 
+  Alignment card1 = Alignment(-0.7, -0.5);
+  Alignment card2 = Alignment(0.0, -0.5);
+  Alignment card3 = Alignment(0.7, -0.5);
+
+  Alignment firstCardState;
+  Alignment secondCardState;
+  Alignment thirdCardState;
+
   @override
   void initState() {
     super.initState();
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft,DeviceOrientation.landscapeRight]);
+    firstCardState = card1;
+    secondCardState = card2;
+    thirdCardState = card3;
   }
+
+  oneToTwo(){
+    setState(() {
+      firstCardState = card2;
+      secondCardState = card1;
+      card2 = secondCardState;
+      card1 = firstCardState;
+    });
+  }
+
+  oneToThree(){
+    setState(() {
+      firstCardState = card3;
+      thirdCardState = card1;
+      card3 = thirdCardState;
+      card1 = firstCardState;
+    });
+  }
+
+  twoToThree(){
+    setState(() {
+      secondCardState = card3;
+      thirdCardState = card2;
+      card3 = thirdCardState;
+      card2 = secondCardState;
+    });
+  }
+
+  Widget getMyCard(){
+    return InkWell(
+      onTap: (){},
+      child: Text(
+        '🂠',
+        style: TextStyle(fontSize: 150),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    //,🂠,🃟
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          // oneToTwo();
+          // twoToThree();
+          // oneToThree();
+        },
+        child: Text('GO'),
+      ),
       appBar: AppBar(),
       body: Stack(
         children: [
-          Text('text1'),
+          AnimatedContainer(
+            duration: Duration(seconds: 1),
+            alignment: firstCardState,
+            child: Container(
+              width: 150,
+              height: 200,
+              color: Colors.red,
+              child: getMyCard(),
+            ),
+          ),
+          AnimatedContainer(
+            duration: Duration(seconds: 1),
+            alignment: secondCardState,
+            child: Container(
+              width: 150,
+              height: 200,
+              color: Colors.green,
+            ),
+          ),
+          AnimatedContainer(
+            duration: Duration(seconds: 1),
+            alignment: thirdCardState,
+            child: Container(
+              width: 150,
+              height: 200,
+              color: Colors.blue,
+            ),
+          )
         ],
       ),
     );
