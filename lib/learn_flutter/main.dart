667c081239565a1ae0803ff16af3d3ce372face7
seongyunLim <lsy96999@gmail.com>
2021-04-20 13:30:24 +0900

    buttons

---
+++ lib/learn_flutter/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyMaterial());
+}
+
+class MyMaterial extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'e4net',
+      home: MyStateless(),
+    );
+  }
+}
+
+
+class MyStateless extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Test App'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('text1'),
+              Text('text2'),
+              Text('text3'),
+              ElevatedButton(
+                  onPressed: (){
+                    print('posts tap!');
+                  },
+                  child: Text('posts'),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 1,
+                    primary: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    onPrimary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.red),
+                      borderRadius: BorderRadius.circular(20)
+                    )
+                  ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: (){},
+                      child: Text('AlertDialog'),
+                  ),
+                  TextButton(
+                      onPressed: (){},
+                      child: Text('snackBar')
+                  ),
+                  FloatingActionButton(
+                    onPressed: (){},
+                    child: Text('Toast'),
+                  )
+                ],
+              ),
+              IconButton(
+                  icon: Icon(Icons.videogame_asset),
+                  onPressed: (){}
+                  )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
